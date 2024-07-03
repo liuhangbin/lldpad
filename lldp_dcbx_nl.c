intel/openlldp

commit da0da5e98508ebb275259059bbd64fb33c83c52e

lldpad: dcbx, map PG up2tc config onto hardware limits
The peer may have more traffic classes then the endstation
can support. In this case the endstation needs to map the
PG attributes onto the available traffic classes.

To do this a minimum of 3 traffic classes are required to
support a link strict class, a PFC enabled class, and
a best effort class.

The mapping scheme maps link strict priorities to the
highest available traffic class, PFC enabled priorities to
the next highest available class, and then stripes best
effort across the remaining traffic classes.

Signed-off-by: John Fastabend <john.r.fastabend@intel.com>

lldp_dcbx_nl.c
@@ -533,7 +533,7 @@ int get_dcb_capabilities(char *ifname,
 	return rval;
 }
 
-int get_dcb_numtcs(char *ifname, u8 *pgtcs, u8 *pfctcs)
+int get_dcb_numtcs(const char *ifname, u8 *pgtcs, u8 *pfctcs)
 {
 	struct nlmsghdr *nlh;
 	struct dcbmsg *d;
@@ -543,6 +543,7 @@ int get_dcb_numtcs(char *ifname, u8 *pgtcs, u8 *pfctcs)
 	unsigned int seq;
 	int i;
 	int found;
+	char name[IFNAMSIZ];
 
 	nlh = start_msg(RTM_GETDCB, DCB_CMD_GNUMTCS);
 	if (NULL == nlh)
@@ -550,7 +551,8 @@ int get_dcb_numtcs(char *ifname, u8 *pgtcs, u8 *pfctcs)
 
 	seq = nlh->nlmsg_seq;
 
-	add_rta(nlh, DCB_ATTR_IFNAME, (void *)ifname, strlen(ifname) + 1);
+	strncpy(name, ifname, sizeof(name));
+	add_rta(nlh, DCB_ATTR_IFNAME, (void *)name, strlen(name) + 1);
 	rta_parent = add_rta(nlh, DCB_ATTR_NUMTCS, NULL, 0);
 
 	rta_child = add_rta(nlh, DCB_NUMTCS_ATTR_ALL, NULL, 0);
@@ -646,17 +648,13 @@ int set_hw_state(char *ifname, int dcb_state)
 */
 int set_hw_pg(char *ifname, pgroup_attribs *pg_data, bool oper_mode)
 {
-	int i;
+	int i, j;
 	int rval = 0;
 	struct tc_config tc[MAX_TRAFFIC_CLASSES];
 	__u8 bwg[MAX_BANDWIDTH_GROUPS];
 	pg_attribs        pg_df_store;
 	pgroup_attribs    pg_df_data, *pg_temp;
 
-
-	/*        code assumes 1:1 up2tc - will need to change when that
-	 *        assumption is invalid
-	 */
 	if (!oper_mode) { /* oper mode is false */
 		get_pg(DEF_CFG_STORE, &pg_df_store);
 		memcpy(&pg_df_data.rx, &pg_df_store.rx, sizeof(pg_df_data.rx));
@@ -666,21 +664,55 @@ int set_hw_pg(char *ifname, pgroup_attribs *pg_data, bool oper_mode)
 		pg_temp = pg_data;
 	}
 
+	/* Configure TX PG per TC Settings */
+	for (i = 0; i < MAX_TRAFFIC_CLASSES; i++)
+		tc[i].up_to_tc_bitmap = 0;
+
+	for (i = 0; i < MAX_USER_PRIORITIES; i++) {
+		for (j = 0; j < MAX_TRAFFIC_CLASSES; j++) {
+			if (pg_temp->tx.up[i].tcmap == j)
+				tc[j].up_to_tc_bitmap |= (1 << i);
+		}
+	}
+
 	for (i = 0; i < MAX_TRAFFIC_CLASSES; i++) {
 		tc[i].pgid = pg_temp->tx.up[i].pgid;
-		tc[i].up_to_tc_bitmap = (u8)(1<<pg_temp->tx.up[i].tcmap);
 		tc[i].prio_type = pg_temp->tx.up[i].strict_priority;
 		tc[i].tc_percent = pg_temp->tx.up[i].percent_of_pg_cap;
 		bwg[i] = pg_temp->tx.pg_percent[i];
+		LLDPAD_DBG("%s %s: (%i) TX pgid %i up_to_tc %i "
+			   "prio %i percent %i\n",
+			    __func__, ifname, i,
+			    tc[i].pgid,
+			    tc[i].up_to_tc_bitmap,
+			    tc[i].prio_type,
+			    tc[i].tc_percent);
 	}
 	rval = set_pg_cfg(ifname, &tc[0], &bwg[0], DCB_CMD_PGTX_SCFG);
 
+	/* Configure RX PG per TC Settings */
+	for (i = 0; i < MAX_TRAFFIC_CLASSES; i++)
+		tc[i].up_to_tc_bitmap = 0;
+
+	for (i = 0; i < MAX_USER_PRIORITIES; i++) {
+		for (j = 0; j < MAX_TRAFFIC_CLASSES; j++) {
+			if (pg_temp->tx.up[i].tcmap == j)
+				tc[j].up_to_tc_bitmap |= (1 << i);
+		}
+	}
+
 	for (i = 0; i < MAX_TRAFFIC_CLASSES; i++) {
 		tc[i].pgid = pg_temp->rx.up[i].pgid;
-		tc[i].up_to_tc_bitmap = (u8)(1<<pg_temp->rx.up[i].tcmap);
 		tc[i].prio_type = pg_temp->rx.up[i].strict_priority;
 		tc[i].tc_percent = pg_temp->rx.up[i].percent_of_pg_cap;
 		bwg[i] = pg_temp->rx.pg_percent[i];
+		LLDPAD_DBG("%s %s: (%i) RX pgid %i up_to_tc %i "
+			   "prio %i percent %i\n",
+			   __func__, ifname, i,
+			   tc[i].pgid,
+			   tc[i].up_to_tc_bitmap,
+			   tc[i].prio_type,
+			   tc[i].tc_percent);
 	}
 	rval |= set_pg_cfg(ifname, &tc[0], &bwg[0], DCB_CMD_PGRX_SCFG);
 	return rval;

dcb_protocol.c
@@ -927,154 +927,158 @@ int dcbx_add_adapter(char *device_name)
 
 	memset (&attr_ptr, 0, sizeof(attr_ptr));
 	attr_ptr.pg = &(attribs.pg);
-	if ((sResult = dcb_check_config(&attr_ptr)) != dcb_success) {
-		LLDPAD_DBG("Rule checker returned error %d\n", sResult);
-	} else { /* big else */
-		memset(&state, 0, sizeof(state));
-		get_dcbx_state(device_name, &state);
+	attr_ptr.pfc = &(attribs.pfc);
 
-		/* Create data stores for the device. */
-		if (!init_dcb_support(device_name, &attribs)) {
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
+	memset(&state, 0, sizeof(state));
+	get_dcbx_state(device_name, &state);
 
-		if (!add_pg(device_name, &attribs.pg)) {
-			LLDPAD_DBG("add_pg error.\n");
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
-		if (!add_pfc(device_name, &attribs.pfc)) {
-			LLDPAD_DBG("add_pfc error.\n");
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
-		if (!add_bwg_desc(device_name, &attribs.descript)) {
-			LLDPAD_DBG("add_bwg_desc error.\n");
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
-		if (!add_control_protocol(device_name, &state)) {
-			LLDPAD_DBG("add_control_protocol error.\n");
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
-		if (!add_peer_pg(device_name)) {
-			LLDPAD_DBG("add_peer_pg error.\n");
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
-		if (!add_peer_pfc(device_name)) {
-			LLDPAD_DBG("add_peer_pfc error.\n");
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
-		if (!add_peer_control_protocol(device_name)) {
-			LLDPAD_DBG("add_peer_control_protocol error.\n");
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
-		if (!add_oper_pg(device_name)) {
-			LLDPAD_DBG("add_oper_pg error.\n");
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
-		if (!add_oper_pfc(device_name)) {
-			LLDPAD_DBG("add_oper_pfc error.\n");
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
-		/* Add APPTLV for supported Subtypes. */
-		for(i = 0; i < DCB_MAX_APPTLV ; i++) {
-			if (!add_apptlv(device_name,
-				&attribs.app[i], i, &state)) {
-				LLDPAD_DBG("add_apptlv error.\n");
-				sResult = dcb_failed;
-				goto add_adapter_error;
-			}
-			if (!add_oper_apptlv(device_name, i)) {
-				LLDPAD_DBG("add_oper_apptlv error.\n");
-				sResult = dcb_failed;
-				goto add_adapter_error;
-			}
-			if (!add_peer_apptlv(device_name, i)) {
-				LLDPAD_DBG("add_peer_apptlv error.\n");
-				sResult = dcb_failed;
-				goto add_adapter_error;
-			}
-		}
-		for(i = 0; i < DCB_MAX_LLKTLV ; i++) {
-			if (!add_llink(device_name, &attribs.llink[i], i)) {
-				LLDPAD_DBG("%s add_llink error.\n",
-					    device_name);
-				sResult = dcb_failed;
-				goto add_adapter_error;
-			}
-			if (!add_oper_llink(device_name, i)) {
-				LLDPAD_DBG("add_oper_llink error.\n");
-				sResult = dcb_failed;
-				goto add_adapter_error;
-			}
-			if (!add_peer_llink(device_name, i)) {
-				LLDPAD_DBG("add_peer_llink error.\n");
-				sResult = dcb_failed;
-				goto add_adapter_error;
-			}
-		}
+	/* Create data stores for the device. */
+	if (!init_dcb_support(device_name, &attribs)) {
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
 
-		if (get_dcb_support(device_name, &dcb_support) != dcb_success) {
+	sResult = dcb_check_config(&attr_ptr);
+	if (sResult != dcb_success)
+		LLDPAD_WARN("Rule checker returned error %d\n", sResult);
+
+	if (!add_pg(device_name, &attribs.pg)) {
+		LLDPAD_DBG("add_pg error.\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	if (!add_pfc(device_name, &attribs.pfc)) {
+		LLDPAD_DBG("add_pfc error.\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	if (!add_bwg_desc(device_name, &attribs.descript)) {
+		LLDPAD_DBG("add_bwg_desc error.\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	if (!add_control_protocol(device_name, &state)) {
+		LLDPAD_DBG("add_control_protocol error.\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	if (!add_peer_pg(device_name)) {
+		LLDPAD_DBG("add_peer_pg error.\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	if (!add_peer_pfc(device_name)) {
+		LLDPAD_DBG("add_peer_pfc error.\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	if (!add_peer_control_protocol(device_name)) {
+		LLDPAD_DBG("add_peer_control_protocol error.\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	if (!add_oper_pg(device_name)) {
+		LLDPAD_DBG("add_oper_pg error.\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	if (!add_oper_pfc(device_name)) {
+		LLDPAD_DBG("add_oper_pfc error.\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	/* Add APPTLV for supported Subtypes. */
+	for (i = 0; i < DCB_MAX_APPTLV; i++) {
+		if (!add_apptlv(device_name,
+			&attribs.app[i], i, &state)) {
+			LLDPAD_DBG("add_apptlv error.\n");
 			sResult = dcb_failed;
 			goto add_adapter_error;
 		}
+		if (!add_oper_apptlv(device_name, i)) {
+			LLDPAD_DBG("add_oper_apptlv error.\n");
+			sResult = dcb_failed;
+			goto add_adapter_error;
+		}
+		if (!add_peer_apptlv(device_name, i)) {
+			LLDPAD_DBG("add_peer_apptlv error.\n");
+			sResult = dcb_failed;
+			goto add_adapter_error;
+		}
+	}
+	for (i = 0; i < DCB_MAX_LLKTLV; i++) {
+		if (!add_llink(device_name, &attribs.llink[i], i)) {
+			LLDPAD_DBG("%s add_llink error.\n",
+				    device_name);
+			sResult = dcb_failed;
+			goto add_adapter_error;
+		}
+		if (!add_oper_llink(device_name, i)) {
+			LLDPAD_DBG("add_oper_llink error.\n");
+			sResult = dcb_failed;
+			goto add_adapter_error;
+		}
+		if (!add_peer_llink(device_name, i)) {
+			LLDPAD_DBG("add_peer_llink error.\n");
+			sResult = dcb_failed;
+			goto add_adapter_error;
+		}
+	}
 
-		/* Initialize features state machines for PG and PFC and
-		 * APPTLVs. */
+	if (get_dcb_support(device_name, &dcb_support) != dcb_success) {
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+
+
+	/* Initialize features state machines for PG and PFC and
+	 * APPTLVs. */
+	if (run_feature_protocol(device_name,
+		DCB_LOCAL_CHANGE_PG, SUBTYPE_DEFAULT) !=
+		dcb_success) {
+		LLDPAD_DBG("run_feature_protocol error (PG)\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	if (run_feature_protocol(device_name,
+		DCB_LOCAL_CHANGE_PFC, SUBTYPE_DEFAULT) != dcb_success) {
+		LLDPAD_DBG("run_feature_protocol error (PFC)\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+	/* If APPTLV subtypes are supported then run the
+	 * feat_prot for those supported subtypes. */
+	for (i = 0; i < DCB_MAX_APPTLV ; i++) {
 		if (run_feature_protocol(device_name,
-			DCB_LOCAL_CHANGE_PG, SUBTYPE_DEFAULT) !=
+			DCB_LOCAL_CHANGE_APPTLV(i), i) !=
 			dcb_success) {
-			LLDPAD_DBG("run_feature_protocol error (PG)\n");
+			LLDPAD_DBG("run_feature_protocol error (APP)\n");
 			sResult = dcb_failed;
 			goto add_adapter_error;
 		}
+	}
+	for (i = 0; i < DCB_MAX_LLKTLV ; i++) {
 		if (run_feature_protocol(device_name,
-			DCB_LOCAL_CHANGE_PFC, SUBTYPE_DEFAULT) != dcb_success) {
-			LLDPAD_DBG("run_feature_protocol error (PFC)\n");
+			DCB_LOCAL_CHANGE_LLINK, i) != dcb_success) {
+			LLDPAD_DBG("run_feature_protocol error (LLINK)\n");
 			sResult = dcb_failed;
 			goto add_adapter_error;
 		}
-		/* If APPTLV subtypes are supported then run the
-		 * feat_prot for those supported subtypes. */
-		for (i = 0; i < DCB_MAX_APPTLV ; i++) {
-			if (run_feature_protocol(device_name,
-				DCB_LOCAL_CHANGE_APPTLV(i), i) !=
-				dcb_success) {
-				LLDPAD_DBG("run_feature_protocol error (APP)\n");
-				sResult = dcb_failed;
-				goto add_adapter_error;
-			}
-		}
-		for (i = 0; i < DCB_MAX_LLKTLV ; i++) {
-			if (run_feature_protocol(device_name,
-				DCB_LOCAL_CHANGE_LLINK, i) != dcb_success) {
-				LLDPAD_DBG("run_feature_protocol error (LLINK)\n");
-				sResult = dcb_failed;
-				goto add_adapter_error;
-			}
-		}
+	}
 
-		EventFlag = 0;
-		DCB_SET_FLAGS(EventFlag, DCB_LOCAL_CHANGE_PG |
-			DCB_LOCAL_CHANGE_PFC | DCB_LOCAL_CHANGE_LLINK);
-		for (i = 0; i < DCB_MAX_APPTLV; i++)
-			DCB_SET_FLAGS(EventFlag, DCB_LOCAL_CHANGE_APPTLV(i));
-		/* Initialize control state machine */
-		if (run_control_protocol (device_name, EventFlag) !=
-			dcb_success) {
-			LLDPAD_DBG("run_control_protocol error.\n");
-			sResult = dcb_failed;
-			goto add_adapter_error;
-		}
-	}	/* big else on top */
+	EventFlag = 0;
+	DCB_SET_FLAGS(EventFlag, DCB_LOCAL_CHANGE_PG |
+	DCB_LOCAL_CHANGE_PFC | DCB_LOCAL_CHANGE_LLINK);
+	for (i = 0; i < DCB_MAX_APPTLV; i++)
+		DCB_SET_FLAGS(EventFlag, DCB_LOCAL_CHANGE_APPTLV(i));
+	/* Initialize control state machine */
+	if (run_control_protocol(device_name, EventFlag) !=
+		dcb_success) {
+		LLDPAD_DBG("run_control_protocol error.\n");
+		sResult = dcb_failed;
+		goto add_adapter_error;
+	}
+
 
 
 add_adapter_error:
@@ -1566,7 +1570,7 @@ void mark_llink_sent(char *device_name, u32 subtype)
 		it->second->protocol.tlv_sent = true;
 }
 
-dcb_result put_pg(char *device_name,  pg_attribs *pg_data)
+dcb_result put_pg(char *device_name, pg_attribs *pg_data, pfc_attribs *pfc_data)
 {
 	full_dcb_attribs 	attribs;
 	full_dcb_attrib_ptrs	attr_ptr;
@@ -1588,6 +1592,8 @@ dcb_result put_pg(char *device_name,  pg_attribs *pg_data)
 		/* Check the rules */
 		memset(&attr_ptr, 0, sizeof(attr_ptr));
 		attr_ptr.pg = pg_data;
+		attr_ptr.pfc = pfc_data;
+
 		if (dcb_check_config(&attr_ptr) != dcb_success) {
 			LLDPAD_DBG("Rule checking failed in put_pg()\n");
 			result = dcb_bad_params;
@@ -2682,11 +2688,16 @@ int set_configuration(char *device_name, u32 EventFlag)
 		if (Oper == NULL || Local == NULL)
 			return dcb_failed;
 
+		Oper->second->num_tcs = Local->second->num_tcs;
+
 		pgroup_attribs pg_data;
 		if (DCB_TEST_FLAGS(EventFlag, DCB_REMOTE_CHANGE_PG,
 			DCB_REMOTE_CHANGE_PG)) {
+			pfc_it op_pfc = pfc_find(&oper_pfc, device_name);
+
 			memset(&attr_ptr, 0, sizeof(attr_ptr));
 			attr_ptr.pg = (Oper->second);
+			attr_ptr.pfc = (op_pfc->second);
 			if ((sResult = dcb_check_config(&attr_ptr))
 				!= dcb_success) {
 				LLDPAD_DBG("  PG rule check returned error %d\n",
@@ -3943,18 +3954,22 @@ dcb_result run_dcb_protocol(char *device_name, u32 EventFlag, u32 Subtype)
 		SubTypeMin = 0;
 		SubTypeMax = DCB_MAX_APPTLV;
 	}
-	/* Run the feature state machines. */
-	if (DCB_TEST_FLAGS(EventFlag, DCB_LOCAL_CHANGE_PG, DCB_LOCAL_CHANGE_PG)
-		&& (result != dcb_ctrl_vers_not_compatible)) {
-		result = run_feature_protocol(device_name,DCB_LOCAL_CHANGE_PG,
-						SUBTYPE_DEFAULT);
-		LocalChange = true;
-	}
+	/* Run the feature state machines:
+	 *
+	 * Order is important PFC must be run before PG features to
+	 * allow up2tc remappings to account for PFC attributes.
+	 */
 	if (DCB_TEST_FLAGS(EventFlag, DCB_LOCAL_CHANGE_PFC,
 		DCB_LOCAL_CHANGE_PFC)
 		&& (result != dcb_ctrl_vers_not_compatible)) {
 		result = run_feature_protocol(device_name,
 			DCB_LOCAL_CHANGE_PFC, SUBTYPE_DEFAULT);
+		LocalChange = true;
+	}
+	if (DCB_TEST_FLAGS(EventFlag, DCB_LOCAL_CHANGE_PG, DCB_LOCAL_CHANGE_PG)
+		&& (result != dcb_ctrl_vers_not_compatible)) {
+		result = run_feature_protocol(device_name, DCB_LOCAL_CHANGE_PG,
+						SUBTYPE_DEFAULT);
 		LocalChange = true;
 	}
 	mask = 0;
@@ -3975,19 +3990,23 @@ dcb_result run_dcb_protocol(char *device_name, u32 EventFlag, u32 Subtype)
 				DCB_LOCAL_CHANGE_LLINK, i);
 		LocalChange = true;
 	}
-	/* Only allow local or remote change at a time. */
+	/* Only allow local or remote change at a time:
+	 *
+	 * Order is important PFC must be run before PG features to
+	 * allow up2tc remappings to account for PFC attributes.
+	 */
 	if (!LocalChange) {
-		if (DCB_TEST_FLAGS(EventFlag, DCB_REMOTE_CHANGE_PG,
-			DCB_REMOTE_CHANGE_PG)
-			&& (result != dcb_ctrl_vers_not_compatible)) {
-			result = run_feature_protocol(device_name,
-				DCB_REMOTE_CHANGE_PG, SUBTYPE_DEFAULT);
-		}
 		if (DCB_TEST_FLAGS(EventFlag, DCB_REMOTE_CHANGE_PFC,
 			DCB_REMOTE_CHANGE_PFC)
 			&& (result != dcb_ctrl_vers_not_compatible)) {
 			result = run_feature_protocol(device_name,
 				DCB_REMOTE_CHANGE_PFC, SUBTYPE_DEFAULT);
+		}
+		if (DCB_TEST_FLAGS(EventFlag, DCB_REMOTE_CHANGE_PG,
+			DCB_REMOTE_CHANGE_PG)
+			&& (result != dcb_ctrl_vers_not_compatible)) {
+			result = run_feature_protocol(device_name,
+				DCB_REMOTE_CHANGE_PG, SUBTYPE_DEFAULT);
 		}
 		mask = 0;
 		for (i = 0; i < DCB_MAX_APPTLV; i++)

dcb_rule_chk.c
@@ -29,6 +29,213 @@
 #include "dcb_protocol.h"
 #include "dcb_rule_chk.h"
 #include "messages.h"
+#include "dcb_types.h"
+
+/**
+ * dcb_fixup_up2tc - resolves mismatch in number of traffic classes
+ * @fixpg: pg attributes or resolve
+ *
+ * Resolve mismatch in number of traffic classes by
+ * grouping traffic types. Requires a minimum of three
+ * class to create a PFC, Link, and Best Effort class.
+ *
+ * Strategy: This takes two passes over the attribs on
+ * the first pass the pg attribs are packed into a
+ * matrix with row index equal to pgid (per_up_list).
+ * Then on the second pass the rows are collapsed onto
+ * the correct number of pgid values (result). Finally,
+ * the new pgid, bw, and tcmap can be tabulated.
+ *
+ * This _should_ happen infrequently so we use up
+ * arrays and variables freely. Any simpler suggestions
+ * would be welcome.
+ */
+static int dcb_fixup_pg(struct pg_attribs *fixpg, struct pfc_attribs *fixpfc)
+{
+	dcb_user_priority_attribs_type * per_up_list[8][8] = { {0} };
+	dcb_user_priority_attribs_type * result[8][8] = { {0} };
+	dcb_user_priority_attribs_type *entry;
+	int index, i, j, k, pgid, bw, cnt, r;
+	int be, pfc, strict, cbe, cpfc, cstrict;
+	int tcbw[8] = {0};
+	int totalbw = 0;
+
+	LLDPAD_INFO("%s : fixup\n", __func__);
+
+	/* Calculate number of pgids used by attributes */
+	for (pgid = 0, i = 0; i < MAX_USER_PRIORITIES; i++) {
+		if (fixpg->tx.up[i].pgid > pgid)
+			pgid = fixpg->tx.up[i].pgid;
+	}
+	pgid++;
+
+	/* If the PGIDs can be mapped onto the number of
+	 * existing traffic classes do it and return
+	 */
+	if (pgid <= fixpg->num_tcs) {
+		for (i = 0; i < MAX_USER_PRIORITIES; i++)
+			fixpg->tx.up[i].tcmap = fixpg->tx.up[i].pgid;
+		return 0;
+	}
+
+	/* Require at least three traffic classes */
+	if (fixpg->num_tcs < MIN_TRAFFIC_CLASSES) {
+		LLDPAD_WARN("DCBX: num_tcs %i!!!\n", fixpg->num_tcs);
+		return -1;
+	}
+
+	/* Build matrix to support attrib manipulation
+	 * this puts attribs with like pgid values in
+	 * the same row of the matrix.
+	 */
+	for (i = 0; i < MAX_USER_PRIORITIES; i++) {
+		index = fixpg->tx.up[i].pgid;
+
+		for (j = 0; j < MAX_USER_PRIORITIES; j++) {
+			if (!per_up_list[index][j]) {
+				per_up_list[index][j] = &fixpg->tx.up[i];
+				break;
+			}
+		}
+	}
+
+	/* Divide up available traffic classes by type */
+	strict = pfc = be = 0;
+
+	for (i = 0; i < MAX_USER_PRIORITIES; i++) {
+		entry = per_up_list[i][0];
+
+		if (!entry)
+			continue;
+
+		pgid = entry->pgid;
+
+		if (entry->strict_priority == dcb_link)
+			strict++;
+		else if (fixpfc && fixpfc->admin[pgid] == pfc_enabled)
+			pfc++;
+		else
+			be++;
+	}
+
+	if (pfc >= fixpg->num_tcs - strict) {
+		pfc = fixpg->num_tcs - strict;
+		if (be)
+			pfc--;
+	}
+	be = fixpg->num_tcs - strict - pfc;
+
+	/* cXXX variables immutable type count */
+	cbe = be;
+	cstrict = strict;
+	cpfc = pfc;
+
+	/* Do REMAPPING into result matrix */
+	for (i = 0; i < MAX_USER_PRIORITIES; i++) {
+		entry = per_up_list[i][0];
+		if (!entry)
+			continue;
+
+		pgid = entry->pgid;
+
+		LLDPAD_INFO("%s : ", __func__);
+		/* Find index, subtract one to account for be, scrict, and
+		 * pfc variables being counters instead of indices.
+		 */
+		if (entry->strict_priority == dcb_link) {
+			index = cbe + cpfc + strict - 1;
+			strict--;
+			LLDPAD_INFO("strict");
+		} else if (fixpfc &&
+			   fixpfc->admin[pgid] == pfc_enabled) {
+			index = cbe + pfc - 1;
+			pfc--;
+			LLDPAD_INFO("pfc");
+		} else {
+			index = be - 1;
+			be--;
+			LLDPAD_INFO("be");
+		}
+
+		tcbw[index] += fixpg->tx.pg_percent[i];
+
+		/* Do row move from old pgid to new pgid */
+		LLDPAD_INFO(" : map %i -> %i\n", i, index);
+		for (k = 1; entry && k < MAX_USER_PRIORITIES; k++) {
+			for (j = 0; j < MAX_USER_PRIORITIES; j++) {
+				if (!result[index][j]) {
+					result[index][j] = entry;
+					break;
+				}
+			}
+			entry = per_up_list[i][k];
+		}
+
+		/* Reset zeroed counters */
+		if (!pfc)
+			pfc = cpfc;
+		if (!strict)
+			strict = cstrict;
+		if (!be)
+			be = cbe;
+	}
+
+	/* The peer _may_ give some percentage of pgpct to a user
+	 * priority that has no pgid mapped to it. This seems like
+	 * a poor config by the peer. However add the bandwidth to
+	 * the highest priority traffic class (that is not strict).
+	 */
+	for (i = 0; i < MAX_USER_PRIORITIES; i++)
+		totalbw += tcbw[i];
+
+	if (totalbw != 100) {
+		index = fixpg->num_tcs - cstrict - 1;
+		tcbw[index] += (100 - totalbw);
+	}
+
+	/* Walk results matrix and update objects */
+	for (i = 0; i < MAX_USER_PRIORITIES; i++) {
+		bw = cnt = 0;
+
+		for (j = 0; j < MAX_USER_PRIORITIES; j++) {
+			entry = result[i][j];
+
+			if (!entry)
+				break;
+
+			entry->pgid = i;
+			entry->tcmap = i;
+			bw += entry->percent_of_pg_cap;
+			cnt++;
+		}
+
+		bw = cnt ?  100 / cnt : 0;
+		r = 100 - (bw * cnt);
+
+		for (j = 0; j < MAX_USER_PRIORITIES; j++) {
+			entry = result[i][j];
+
+			if (!entry)
+				break;
+
+			if (entry->strict_priority == dcb_link)
+				entry->percent_of_pg_cap = 0;
+			else
+				entry->percent_of_pg_cap = bw;
+		}
+
+		if (j) {
+			entry = result[i][--j];
+			if (entry)
+				entry->percent_of_pg_cap += r;
+		}
+	}
+
+	for (i = 0; i < MAX_TRAFFIC_CLASS; i++)
+		fixpg->tx.pg_percent[i] = tcbw[i];
+
+	return 0;
+}
 
 /******************************************************************************
  * This function checks DCB rules for DCBs settings.
@@ -56,6 +263,8 @@ dcb_check_config (full_dcb_attrib_ptrs *attribs)
 		return dcb_failed;
 
 	if (attribs->pg) {
+		int err;
+
 		pg = attribs->pg;
 		memset(tx_bw_sum,0,sizeof(tx_bw_sum));
 		memset(rx_bw_sum,0,sizeof(rx_bw_sum));
@@ -63,6 +272,13 @@ dcb_check_config (full_dcb_attrib_ptrs *attribs)
 		memset(rx_link_strict,0,sizeof(rx_link_strict));
 
 		tx_bw = 0, rx_bw = 0;
+
+		err = dcb_fixup_pg(pg, attribs->pfc);
+		if (err) {
+			LLDPAD_WARN("dcb_fixup_pg returned error %i\n", err);
+			return dcb_failed;
+		}
+
 		/* Internally in the pg_attribs structure, a link strict PGID is 
 		 * maintained as a PGID value (0-7) with a corresponding
 		 * strict_priority field value of 'dcb_link'.  Only one link strict

include/dcb_driver_interface.h
@@ -48,6 +48,6 @@ int set_hw_all(char *device_name);
 int get_dcb_capabilities(char *device_name, struct feature_support *dcb_capabilites);
 u32 double_to_fixpt_int(double double_val);
 
-int get_dcb_numtcs(char *device_name, u8 *pgtcs, u8 *pfctcs);
+int get_dcb_numtcs(const char *device_name, u8 *pgtcs, u8 *pfctcs);
 #endif /* _DCB_DRIVER_INTERFACE_H_ */
 

include/dcb_protocol.h
@@ -91,7 +91,8 @@ dcb_result get_peer_control(char *device_name,
 dcb_result put_peer_control(char *, control_protocol_attribs *);
 
 dcb_result get_pg(char *device_name, pg_attribs *pg_data);
-dcb_result put_pg(char *device_name, pg_attribs *pg_data);
+dcb_result put_pg(char *device_name, pg_attribs *pg_data,
+		  pfc_attribs *pfc_data);
 dcb_result put_peer_pg(char *,  pg_attribs *);
 dcb_result get_oper_pg(char *device_name, pg_attribs *pg_data);
 dcb_result get_peer_pg(char *device_name, pg_attribs *pg_data);

include/dcb_types.h
@@ -33,6 +33,7 @@
 #define MAX_USER_PRIORITIES       8
 #define MAX_BANDWIDTH_GROUPS      8
 #define MAX_TRAFFIC_CLASSES       8
+#define MIN_TRAFFIC_CLASSES       3
 #define SHORT_STRING              20
 #define MAX_DESCRIPTION_LEN       100
 

lldp_dcbx_cfg.c
@@ -35,6 +35,7 @@
 #include "config.h"
 #include "libconfig.h"
 #include "lldp_util.h"
+#include "dcb_driver_interface.h"
 
 #define CFG_VERSION "1.0"
 
@@ -579,7 +580,7 @@ set_error:
 	return dcb_failed;
 }
 
-static int get_default_persistent(full_dcb_attribs *attribs)
+static int get_default_persistent(const char *ifname, full_dcb_attribs *attribs)
 {
 	int i;
 
@@ -589,11 +590,12 @@ static int get_default_persistent(full_dcb_attribs *attribs)
 	if (get_pfc(DEF_CFG_STORE, &attribs->pfc) != dcb_success)
 		return 1;
 
+	get_dcb_numtcs(ifname, &attribs->pg.num_tcs, &attribs->pfc.num_tcs);
+
 	for (i = 0; i < DCB_MAX_APPTLV; i++) {
 		if (get_app(DEF_CFG_STORE, i, &attribs->app[i]) != dcb_success)
 			return 1;
 	}
-
 
 	for (i = 0; i < DCB_MAX_LLKTLV; i++) {
 		if (get_llink(DEF_CFG_STORE, i,
@@ -643,6 +645,7 @@ int get_persistent(char *device_name, full_dcb_attribs *attribs)
 	config_setting_t *setting_array = NULL;
 	config_setting_t *setting_traffic = NULL;
 	config_setting_t *setting_value = NULL;
+	full_dcb_attrib_ptrs attrib_ptrs;
 	int result = dcb_failed, i;
 	int results[MAX_USER_PRIORITIES];
 	int len;
@@ -653,7 +656,7 @@ int get_persistent(char *device_name, full_dcb_attribs *attribs)
 	eth_settings = config_setting_get_member(dcbx_setting, device_name);
 
 	/* init the internal data store for device_name */
-	result = get_default_persistent(attribs);
+	result = get_default_persistent(device_name, attribs);
 	if (NULL == eth_settings) {
 		assert(memcmp(device_name, DEF_CFG_STORE, 
 			strlen(DEF_CFG_STORE)));
@@ -907,12 +910,17 @@ int get_persistent(char *device_name, full_dcb_attribs *attribs)
 			goto set_default;
 	}
 
-
 	return 0;
 
 set_default:
 	LLDPAD_WARN("Error read config file.\n");
-	result = get_default_persistent(attribs);
+	result = get_default_persistent(device_name, attribs);
+
+	if (!result) {
+		attrib_ptrs.pg = &attribs->pg;
+		result = dcb_check_config(&attrib_ptrs);
+	}
+
 	return result;
 }
 

lldp_dcbx_cmds.c
@@ -127,20 +127,22 @@ static int get_arg_tlvtxenable(struct cmd *cmd, char *arg, char *argvalue,
 
 void dont_advertise_dcbx_all(char *ifname, bool ad)
 {
-	int i;
+	int i, is_pfc;
 	pfc_attribs pfc_data;
 	pg_attribs pg_data;
 	app_attribs app_data;
 	llink_attribs llink_data;
 	u32 event_flag = 0;
 
+	is_pfc = get_pfc(ifname, &pfc_data);
+
 	if (get_pg(ifname, &pg_data) == dcb_success) {
 		pg_data.protocol.Advertise = ad;
-		put_pg(ifname, &pg_data);
+		put_pg(ifname, &pg_data, &pfc_data);
 		event_flag |= DCB_LOCAL_CHANGE_PG;
 	}
 
-	if (get_pfc(ifname, &pfc_data) == dcb_success) {
+	if (is_pfc == dcb_success) {
 		pfc_data.protocol.Advertise = ad;
 		put_pfc(ifname, &pfc_data);
 		event_flag |= DCB_LOCAL_CHANGE_PFC;
@@ -880,10 +882,11 @@ static int get_llink_data(llink_attribs *llink_data, int cmd, char *port_id,
 static int set_pg_config(pg_attribs *pg_data, char *port_id, char *ibuf,
 	int ilen)
 {
+	pfc_attribs pfc_data;
 	full_dcb_attrib_ptrs dcb_data;
 	u8 flag;
 	dcb_result status = dcb_success;
-	int i;
+	int i, is_pfc;
 	int plen;
 	int off;
 	bool used[MAX_BANDWIDTH_GROUPS];
@@ -1013,7 +1016,12 @@ static int set_pg_config(pg_attribs *pg_data, char *port_id, char *ibuf,
 		return status;
 	}
 
-	status = put_pg(port_id, pg_data);
+	is_pfc = get_pfc(port_id, &pfc_data);
+	if (is_pfc == dcb_success)
+		status = put_pg(port_id, pg_data, &pfc_data);
+	else
+		status = put_pg(port_id, pg_data, NULL);
+
 	if (status != dcb_success)
 		printf("error[%d] setting PG data for %s\n", status, port_id);
 	

liuhangbin/lldpad

commit 7bd359d33fb73b688a97d211255a69f5f3dd6ffb

lldp_dcbx_nl: Fix Copy-Paste Typo
It should check the RX PGID when configuring RX PG per TC settings.

Fixes: da0da5e98508 ("lldpad: dcbx, map PG up2tc config onto hardware limits")
Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>

lldp_dcbx_nl.c
@@ -709,7 +709,7 @@ int set_hw_pg(char *ifname, pgroup_attribs *pg_data, bool oper_mode)
 
 	for (i = 0; i < MAX_USER_PRIORITIES; i++) {
 		for (j = 0; j < MAX_TRAFFIC_CLASSES; j++) {
-			if (pg_temp->tx.up[i].pgid == j) {
+			if (pg_temp->rx.up[i].pgid == j) {
 				tc[j].up_to_tc_bitmap |= (1 << i);
 				tc[j].prio_type = pg_temp->rx.up[i].strict_priority;
 				tc[j].tc_percent = pg_temp->rx.up[i].percent_of_pg_cap;